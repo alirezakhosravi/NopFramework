85f565144503bf3753c99b0567cc6c81b51d645f
alirezakhosravi <alirezakhosravi@live.com>
2018-10-01 09:42:16 +0330

    some bug fixed

--- Libraries/Nop.Data/DesignTimeDbContextFactory.cs
+++ Libraries/Nop.Data/DesignTimeDbContextFactory.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Nop.Core.Data;
+using Nop.Core.Infrastructure;
 
 namespace Nop.Data
 {
+    /// <summary>
+    /// Design time db context factory.
+    /// </summary>
     public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<NopObjectContext>
     {
+        /// <summary>
+        /// Creates the db context.
+        /// </summary>
+        /// <returns>The db context.</returns>
         public NopObjectContext CreateDbContext(string[] args)
         {
             //power shell -> Add-Migration migration-name
@@ -13,17 +23,37 @@ namespace Nop.Data
 
             //consule -> dotnet ef database update
 
-            var optionsBuilder = new DbContextOptionsBuilder<NopObjectContext>();
-            optionsBuilder
-                .UseSqlServer("Data Source=localhost;Initial Catalog=nopFramework;Integrated Security=False;Persist Security Info=False;User ID=sa;Password=123qweRt"
-                              , sqlOptions =>
-                              {
-                                  sqlOptions.EnableRetryOnFailure(
-                                  maxRetryCount: 10,
-                                  maxRetryDelay: TimeSpan.FromSeconds(30),
-                                  errorNumbersToAdd: null);
-                              });
+            var nopFileProvider = EngineContext.Current.Resolve<INopFileProvider>();
+            string connection = DataSettingsManager.LoadSettings(nopFileProvider.MapPath("~/App_Data/dataSettings.json"), true).DataConnectionString;
 
+            if (string.IsNullOrEmpty(connection))
+            {
+                //this connection string used when programmer need forec start migration as design time
+                connection = "Data Source=localhost;Initial Catalog=nopFramework;Integrated Security=False;Persist Security Info=False;User ID=sa;Password=123qweRt";
+            }
+
+            var optionsBuilder = new DbContextOptionsBuilder<NopObjectContext>();
+            try
+            {
+                //test connection string
+                var _ = new SqlConnectionStringBuilder(connection);
+
+                optionsBuilder
+                    .UseSqlServer(connection
+                                  , sqlOptions =>
+                                  {
+                                      sqlOptions.EnableRetryOnFailure(
+                                      maxRetryCount: 10,
+                                      maxRetryDelay: TimeSpan.FromSeconds(30),
+                                      errorNumbersToAdd: null);
+                                  });
+            }
+#pragma warning disable RECS0022 // A catch clause that catches System.Exception and has an empty body
+            catch
+#pragma warning restore RECS0022 // A catch clause that catches System.Exception and has an empty body
+            {
+                // do noting
+            }
             return new NopObjectContext(optionsBuilder.Options);
         }
     }
